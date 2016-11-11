b526f22b49f0f12d56de65aa1d7ef0a12d0cc3c1
gu-ramosp <gu.ramosp@gmail.com>
2016-11-10 22:25:32 -0200

    Alterações na ULA
    Ajeitei a ULA pra branchs, mas ainda tem o problema do tamanho do
    resultado da multiplicação

--- processador/ula.vhdl
+++ processador/ula.vhdl
@@ -10,40 +10,50 @@ entity ula is
 			entrada0 : in unsigned(15 downto 0);
 			entrada1 : in unsigned(15 downto 0);
 			selecao  : in unsigned(2 downto 0);
+			
 			saida    : out unsigned(15 downto 0);
-			estado   : out unsigned(2 downto 0)
+			estado   : out unsigned(1 downto 0)
 		);
 end entity;
---estado da operação
--- 000 outros casos
--- 001 a conta deu zero
--- 010 a conta deu positivo
--- 011 a conta deu negativo
+-- Estado da operação
+-- 00:os dois são iguais
+-- 01: maior
+-- 10: menor
 
+-- Seleçãp
 -- 000 soma
 -- 001 subtração
 -- 010 multiplicação	!!NAO TEM
 -- 011 divisão
--- 100 maior		!!NAO TEM
--- 101 é zero		!!NAO TEM
--- 110 é igual		!!NAO TEM
+-- 100 maior		
+-- 101 é zero		
+-- 110 é igual		
 
 architecture a_ula of ula is
+
+	signal cmp_en: std_logic; --da enable apenas se for feita uma comparação
+	
 	begin
 		saida <=
 			entrada0 + entrada1 when selecao = "000" else
 			entrada0-entrada1 when selecao = "001" else
-			--(entrada0 * entrada1) when selecao = "010" else
-			--multiplicacao da um problemao, retorna 32 bits em vez de 16
+			entrada0 * entrada1 when selecao = "010" else --multiplicacao da um problemao, retorna 32 bits em vez de 16
 			entrada0/entrada1 when selecao = "011"
 			else "0000000000000000";
-
-		estado <=
-			"001" when saida = "0000000000000000" else
-			"010" when saida(15 downto 14) = '0' else
-			"011" when saida(15 downto 14) = '1'
-			else "000";
-
+		
+		-- Compara os dois registradores e guarda a resposta em resposta_p_mux
+		cmp_en <=
+			'1' when selecao = "100" else
+			'1' when selecao = "101" else
+			'1' when selecao = "110" else
+			'0';
+		
+		-- Multiplexador para os branchs
+		estado <= "00" when entrada0 = entrada1 and cmp_en = '1' else
+				  "01" when entrada0 > entrada1 and cmp_en = '1' else
+				  "10" when entrada0 < entrada1 and cmp_en = '1' else
+				  "11";
+	
 end architecture;
 
 
--- processador/ula_tb.vhdl
+++ processador/ula_tb.vhdl
@@ -1,5 +1,3 @@
---
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -9,139 +7,74 @@ end;
 
 architecture a_ula_tb of ula_tb is
 	component ula
-		port(
+		port
+		(
 			entrada0 : in unsigned(15 downto 0);
 			entrada1 : in unsigned(15 downto 0);
-			selecao: in unsigned(2 downto 0);
-			saida : out unsigned(15 downto 0);
-			maior : out std_logic;
-			igual : out std_logic;
-			zero : out std_logic
+			selecao  : in unsigned(2 downto 0);
+			
+			saida    : out unsigned(15 downto 0);
+			estado   : out unsigned(1 downto 0)
 		);
 	end component;
-
--- 000 soma
--- 001 subtração
--- 010 multiplicação
--- 011 divisão
--- 100 maior
--- 101 é zero
--- 110 é igual
-
-	signal maior, igual, zero : std_logic;
+	
+	signal estado   : unsigned(1 downto 0);
 	signal selecao: unsigned (2 downto 0);
 	signal saida, entrada0, entrada1: unsigned(15 downto 0);
-
+	
 	begin 
 		utt: ula port map(
 							entrada0 => entrada0,
 							entrada1 => entrada1,
 							selecao => selecao,
 							saida => saida,
-							maior => maior,
-							igual => igual,
-							zero => zero
+							estado => estado
 						);
-
+						
 	process
 		begin
-
-			entrada0 <= "0000000001010100";--84
-			entrada1 <= "0000000000001101";--15
-			selecao <= "000";	--soma
+			
+			entrada0 <= "0000000001010100"; --84
+			entrada1 <= "0000000000001101";  --15
+			selecao <= "100";
 			wait for 50 ns;
-			entrada0 <= "0000000001010100";--84
-			entrada1 <= "0000000000001101";--15
-			selecao <= "001";	--subtracao 
+			entrada0 <= "0000000001010100"; --84
+			entrada1 <= "0000000000001101";  --15
+			selecao <= "101";
 			wait for 50 ns;
-			entrada0 <= "0000000000001101";--15
-			--entrada1 <= "0000000001010100";--84
-			entrada1 <= "0000000000000001";--1
-			selecao <= "001";	--subtracao com resultado negativo
+			entrada0 <= "0000000001010100"; --84
+			entrada1 <= "0000000000001101";  --15
+			selecao <= "100";
 			wait for 50 ns;
-			entrada0 <= "0000000000000100";--4
-			entrada1 <= "0000000000001101";--15
-			selecao <= "010";	--multiplicacao
+			entrada0 <= "0000000001010100"; --84
+			entrada1 <= "0000000000001101";  --15
+			selecao <= "110";
 			wait for 50 ns;
-			entrada0 <= "0000000001010100";--84
-			entrada1 <= "0000000000001101";--15
-			selecao <= "011";	--divisao
+			
+			entrada0 <= "0101011111111111"; --22527
+			entrada1 <= "0101011111111111"; -- 65632
+			selecao <= "110";
 			wait for 50 ns;
-			entrada0 <= "0000000001010100";--84
-			entrada1 <= "0000000000001101";--15
-			selecao <= "100";	--maior
+			entrada0 <= "0101011111111111"; 
+			entrada1 <= "1111111101011100"; 
+			selecao <= "110";
 			wait for 50 ns;
-			entrada0 <= "0000000000001101";--15
-			entrada1 <= "0000000001010100";--84
-			selecao <= "100";	--maior
+			entrada0 <= "0101011111111111"; 
+			entrada1 <= "1111111101011100"; 
+			selecao <= "110";
 			wait for 50 ns;
-			entrada0 <= "0000000001010100";--84
-			entrada1 <= "0000000000001101";--15
-			selecao <= "101";	--zero
+			entrada0 <= "0101011111111111"; 
+			entrada1 <= "1111111101011100"; 
+			selecao <= "110";
 			wait for 50 ns;
-			entrada0 <= "0000000000000000";--84
-			entrada1 <= "0000000000001101";--15
-			selecao <= "101";	--zero
-			wait for 50 ns;
-			entrada0 <= "0000000001010100";--84
-			entrada1 <= "0000000000001101";--15
-			selecao <= "110";	--igual
-			wait for 50 ns;
-			entrada0 <= "0000000000001101";--15
-			entrada1 <= "0000000000001101";--15
-			selecao <= "110";	--igual
-			wait for 50 ns;
-
-
-			entrada0 <= "0000000000000000";
-			entrada1 <= "0000000000000000";
-			selecao <= "000";
-			wait for 200 ns;
-
-
-			entrada0 <= "0111111110000101";--32645
-			entrada1 <= "0000000000000010";--2
-			selecao <= "000";	--soma
-			wait for 50 ns;
-			entrada0 <= "0111111110000101";--32645
-			entrada1 <= "0000000000000010";--2
-			selecao <= "001";	--subtracao 
-			wait for 50 ns;
-			entrada0 <= "0111111110000101";--32645
-			entrada1 <= "0000000000000010";--2
-			selecao <= "010";	--multiplicacao
-			wait for 50 ns;
-			entrada0 <= "0111111110000101";--32645
-			entrada1 <= "0000000000000010";--2
-			selecao <= "011";	--divisao
-			wait for 50 ns;
-			entrada0 <= "0111111110000101";--32645
-			entrada1 <= "0000000000000010";--2
-			selecao <= "100";	--maior
-			wait for 50 ns;
-			entrada0 <= "0000000000000010";--2
-			entrada1 <= "0111111110000101";--32645
-			selecao <= "100";	--maior
-			wait for 50 ns;
-			entrada0 <= "0111111110000101";--32645
-			entrada1 <= "0000000000000000";--0
-			selecao <= "101";	--zero
-			wait for 50 ns;
-			entrada0 <= "0000000000000000";--0
-			entrada1 <= "0000000000000001";--1
-			selecao <= "101";	--zero
-			wait for 50 ns;
-			entrada0 <= "0111111110000101";--32645
-			entrada1 <= "0000000000001101";--15
-			selecao <= "110";	--igual
-			wait for 50 ns;
-			entrada0 <= "0111111110000101";--32645
-			entrada1 <= "0111111110000101";--32645
-			selecao <= "110";	--igual
-			wait for 50 ns;
-
-
+		
+			
+			
 			wait;
-
+			
+			
+			
+			
 		end process;
 end architecture;
+			
